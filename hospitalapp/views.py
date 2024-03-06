ca7623bd2425807b22925da8dbf53901d89b15df
Corel-dev <tony.kyalo06@gmail.com>
2024-03-06 10:10:23 +0300

    fifth commit

--- hospitalapp/views.py
+++ hospitalapp/views.py
@@ -1,6 +1,12 @@
+import json
+
+import requests
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from requests.auth import HTTPBasicAuth
 
 from Hospitalmanagementsys.forms import ImageUploadForm
+from hospitalapp.credentials import MpesaAccessToken, LipanaMpesaPpassword
 from hospitalapp.models import Members, appointment, Products, ImageModel
 
 
@@ -61,6 +67,7 @@ def adminhome(request):
     else:
         return render(request, 'Log in.html')
 
+
 def upload_image(request):
     if request.method == 'POST':
         form = ImageUploadForm(request.POST, request.FILES)
@@ -71,11 +78,54 @@ def upload_image(request):
         form = ImageUploadForm()
     return render(request, 'up load.html', {'form': form})
 
+
 def show_image(request):
     images = ImageModel.objects.all()
     return render(request, 'showimages.html', {'images': images})
+
 
 def imagedelete(request, id):
     image = ImageModel.objects.get(id=id)
     image.delete()
     return redirect('/showimage')
+
+
+def token(request):
+    consumer_key = 'G2da0UuIwYUkm1cW9LfhMtiJKoPjzcsmCRZG8njpVx8sM7vW'
+    consumer_secret = 'Butuz6n7FCN6Vkh7f95Om06IAQcDKPgdNhUgoAEvCLzhAlr8x4Bs2UC9YWfs1oEH'
+    api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+
+    r = requests.get(api_URL, auth=HTTPBasicAuth(
+        consumer_key, consumer_secret))
+    mpesa_access_token = json.loads(r.text)
+    validated_mpesa_access_token = mpesa_access_token["access_token"]
+
+    return render(request, 'token.html', {"token": validated_mpesa_access_token})
+
+
+def pay(request):
+    return render(request, 'pay.html')
+
+
+def stk(request):
+    if request.method == "POST":
+        phone = request.POST['phone']
+        amount = request.POST['amount']
+        access_token = MpesaAccessToken.validated_mpesa_access_token
+        api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+        headers = {"Authorization": "Bearer %s" % access_token}
+        request = {
+            "BusinessShortCode": LipanaMpesaPpassword.Business_short_code,
+            "Password": LipanaMpesaPpassword.decode_password,
+            "Timestamp": LipanaMpesaPpassword.lipa_time,
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": amount,
+            "PartyA": phone,
+            "PartyB": LipanaMpesaPpassword.Business_short_code,
+            "PhoneNumber": phone,
+            "CallBackURL": "https://sandbox.safaricom.co.ke/mpesa/",
+            "AccountReference": "Apen Softwares",
+            "TransactionDesc": "Web Development Charges"
+        }
+        response = requests.post(api_url, json=request, headers=headers)
+        return HttpResponse("Success")
